f9113460178bafe20833895af76b5f04127cd6e5
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-26 10:58:12 +0900

    Add project files.

---
+++ FreshBox/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FreshBox
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
