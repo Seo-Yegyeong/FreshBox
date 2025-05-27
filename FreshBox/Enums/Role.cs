e5a79d4edc09e68ca9570a04fdf4de72650f6c0e
ChoiMari <chl98639863@gmail.com>
2025-05-27 22:55:57 +0900

    Enum 정의

---
+++ FreshBox/Enums/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshBox.Enums
+{
+    /// <summary>
+    /// MEMBER 테이블의 role컬럼 타입
+    /// </summary>
+    public enum Role
+    {
+        Employee, //사원
+        Admin //관리자
+    }
+}
