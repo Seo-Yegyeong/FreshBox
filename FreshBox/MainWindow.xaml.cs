b41616b2196d53437303e106be13bc528037aeb4
ChoiMari <chl98639863@gmail.com>
2025-05-29 11:33:55 +0900

    메인창 백지로

--- FreshBox/MainWindow.xaml.cs
+++ FreshBox/MainWindow.xaml.cs
@@ -25,28 +25,5 @@ namespace FreshBox
             InitializeComponent();
         }
 
-
-        // DB 연결 테스트 버튼 클릭 이벤트 핸들러예요!
-        private void TestDbConnection_Click(object sender, RoutedEventArgs e)
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
-
-            try
-            {
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                {
-                    conn.Open();
-
-                    string query = "SELECT COUNT(*) FROM member";
-                    MySqlCommand cmd = new MySqlCommand(query, conn);
-                    var count = Convert.ToInt32(cmd.ExecuteScalar());
-                    MessageBox.Show($"사용자 수: {count}");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("❌ DB 연결 실패: " + ex.Message);
-            }
-        }
     }
 }
