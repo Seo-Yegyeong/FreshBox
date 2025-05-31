b7989ce9bc348cd44ca02dcf4fc94f6442ee661f
ChoiMari <chl98639863@gmail.com>
2025-05-31 10:27:31 +0900

    bool타입으로 username중복검사 메서드 리턴타입 변경

--- FreshBox/Services/MemberService.cs
+++ FreshBox/Services/MemberService.cs
@@ -1,6 +1,7 @@
 ﻿using FreshBox.Repository;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,27 +18,32 @@ namespace FreshBox.Services
         /// </summary>
         /// <param name="username">사용자가 입력한 사용자 ID(username)</param>
         /// <returns>
-        /// 중복 검사 결과에 따른 메시지를 반환합니다.
-        /// - 중복이 없으면 "사용하실 수 있는 ID입니다."
-        /// - 중복이 있으면 "사용할 수 없는 아이디입니다. 다른 아이디를 입력해 주세요."
-        /// - 에러 발생 시 적절한 오류 메시지를 반환합니다.
+        /// username 중복 검사 결과를 반환합니다
+        /// 중복 시  : true반환
+        /// 중복 없음(사용 가능) : false반환
         /// </returns>
-        public string CheckUsernameDuplicate(string username) {
+        public bool IsUsernameDuplicate(string username) {
 
-            int result = memberRepo.FindByUsername(username);
+            try
+            {
+                int result = memberRepo.FindByUsername(username);
 
-            if (result == 0) // 중복 없음
-            {
-                return "사용하실 수 있는 ID입니다.";
+                if (result == 0) // 중복 없음
+                {
+                    return false;
+                }
+                else if (result == 1) // DB에 중복된 username 있음
+                {
+                    return true;
+                }
+                else
+                    throw new Exception("알 수 없는 DB 결과"); //호출부로 예외 던짐
             }
-            else if (result == 1) // DB에 중복된 username 있음
-            {
-                return "사용할 수 없는 아이디입니다. 다른 아이디를 입력해 주세요.";
+            catch (Exception ex) {
+                Debug.WriteLine($"Error: {ex.Message}");
+                throw; // 호출부로 예외 던짐
             }
-            else { // 에러 
-                // 네트워크, DB 연결 문제 등 예외 상황에 대한 사용자 안내 메시지
-                return "Error : 서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.";
-            }           
+
         }
 
 
