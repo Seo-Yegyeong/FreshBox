e8becc9518d75f8dbd74a02d7c1c288157387fb1
ChoiMari <chl98639863@gmail.com>
2025-06-01 21:19:21 +0900

    디버그 출력 문구 변경함

--- FreshBox/Services/SignupService.cs
+++ FreshBox/Services/SignupService.cs
@@ -1,4 +1,5 @@
 ﻿using FreshBox.Repository;
+using MySqlX.XDevAPI.Common;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -36,14 +37,51 @@ namespace FreshBox.Services
                 {
                     return true;
                 }
-                else
-                    throw new Exception("알 수 없는 DB 결과"); //호출부로 예외 던짐
+                else {
+                    throw new Exception("ERROR : DB 예외발생"); //호출부로 예외 던짐
+                }
+                    
             }
             catch (Exception ex) {
                 Debug.WriteLine($"Error: {ex.Message}");
                 throw; // 호출부로 예외 던짐
             }
 
+        }
+
+        /// <summary>
+        /// phone 중복 검사 서비스 메서드
+        /// </summary>
+        /// <param name="phoneNum">유효성 검사가 완료된 사용자 입력값</param>
+        /// <returns>true 중복된 번호(사용 불가), false 중복 없음(사용 가능)</returns>
+        public bool IsPhoneDuplicate(string phoneNum) {
+            
+            bool isDuplicate; // 코드 이해 돕기위해 결과를 변수에 담음
+
+            try {
+                int result = memberRepo.FindByPhoneNum(phoneNum);
+                // ㄴ 리턴 값 1이면 중복 있음 , 0이면 중복 없음
+
+                if (result == 0)
+                { // 중복 없음
+                    isDuplicate = false; // 중복입니까? 아니요
+                    return isDuplicate;
+                }
+                else if (result == 1)
+                {
+                    isDuplicate = true; // 중복 입니까? 네
+                    return isDuplicate;
+                }
+                else {
+                    throw new Exception($"[예외] [SignUpService.IsPhoneDuplicate]" +
+                        $" Unexpected DB result in FindByPhoneNum: {result}"); 
+                    // 호출부로 예외 던짐
+                }
+            }
+            catch(Exception ex) {
+                Debug.WriteLine($"[예외] [SignUpService.IsPhoneDuplicate] Error: {ex.Message}");
+                throw; // 호출부로 예외 던짐
+            }
         }
 
 
