ca0db9bb39ae41d6bd4133487c0a499cd9f72744
ChoiMari <chl98639863@gmail.com>
2025-06-07 02:57:22 +0900

    뒤로가기 메서드 정의

---
+++ FreshBox/ViewModels/WorkMenuViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FreshBox.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshBox.ViewModels
+{
+    /// <summary>
+    /// WorkMenuView와 바인딩 된 뷰모델 클래스
+    /// </summary>
+    public partial class WorkMenuViewModel : ObservableObject
+    {
+
+        /// <summary>
+        /// 뒤로가기 클릭 시 실행되는 메서드
+        /// 이전에 보았던 화면으로 돌아감
+        /// </summary>
+        [RelayCommand]
+        private void GoToBack() { 
+            ViewNavigationService.Instance.GoBack();
+        }
+    }
+}
