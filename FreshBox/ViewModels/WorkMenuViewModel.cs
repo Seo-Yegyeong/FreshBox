b02dd6ed36a08663f845af2be93d4ad85cdd8c80
ChoiMari <chl98639863@gmail.com>
2025-06-07 04:24:57 +0900

    UI에서 권한 변경에 따른 버튼의 가시성 설정 위해서 싱글턴으로 관리

--- FreshBox/ViewModels/WorkMenuViewModel.cs
+++ FreshBox/ViewModels/WorkMenuViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using FreshBox.Enums;
 using FreshBox.Services;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FreshBox.ViewModels
 {
@@ -14,13 +17,71 @@ namespace FreshBox.ViewModels
     /// </summary>
     public partial class WorkMenuViewModel : ObservableObject
     {
+        // 싱글턴으로 관리
+        private static WorkMenuViewModel? instance;
+        public static WorkMenuViewModel Instance
+        {
+            get
+            {
+                if (instance == null) {
+                    instance = new WorkMenuViewModel();
+                }  
+                return instance;
+            }
+        }
+
+        // 생성자는 private으로 외부 생성 제한
+        private WorkMenuViewModel()
+        {
+            // 초기화 로직
+        }
+
+        // 권한 갱신 후 UI 갱신 메서드
+        public void RefreshRoleProperties()
+        {
+            OnPropertyChanged(nameof(IsCommonButtonVisible));
+            OnPropertyChanged(nameof(IsAdminVisible));
+        }
+
+
+        // 프로퍼티 선언 ---------------------------
 
         /// <summary>
-        /// 뒤로가기 클릭 시 실행되는 메서드
+        /// 로그인한 사용자의 권한(읽기 전용 속성)
+        /// 이 값을 읽으려고 하면 내부에서 LoginSession.GetInstance().LoggedInRole 값을 반환함
+        /// LoginSession 싱글턴 객체의 LoggedInRole 값을 그대로 제공해주는 역할
+        /// </summary>
+        public Role LoggedInRole
+        {
+            get
+            {
+                return LoginSession.GetInstance().LoggedInRole;
+            }
+        }
+
+        // 사원 및 관리자 공통 버튼 표시
+        public Visibility IsCommonButtonVisible => (LoggedInRole == Role.Employee || LoggedInRole == Role.Admin) ? Visibility.Visible : Visibility.Collapsed;
+        
+        // 관리자 전용 버튼 표시
+        public Visibility IsAdminVisible => LoggedInRole == Role.Admin ? Visibility.Visible : Visibility.Collapsed;
+
+        /*
+         * WPF에서 UI 요소의 Visibility 속성
+         * Collapsed: 요소가 화면에 안 보이고, 자리도 차지하지 않음
+            → UI에서 완전히 사라진 상태
+         * Hidden: 요소가 안 보이지만, 공간은 차지함
+         * Visible: 요소가 보임
+         */
+
+
+        // 화면 전환 메서드 선언 -----------------------------------
+
+        /// <summary>
+        /// 뒤로가기 버튼 클릭 시 호출됨
         /// 이전에 보았던 화면으로 돌아감
         /// </summary>
         [RelayCommand]
-        private void GoToBack() { 
+        private void GoToBack() {
             ViewNavigationService.Instance.GoBack();
         }
     }
