5885e539001e6bc1988a052745282f70d511da7c
ChoiMari <chl98639863@gmail.com>
2025-06-07 04:27:03 +0900

    뒷코드에서 싱글턴으로 뷰모델과 연결

--- FreshBox/Views/WorkMenuView.xaml.cs
+++ FreshBox/Views/WorkMenuView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FreshBox.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace FreshBox.Views
         public WorkMenuView()
         {
             InitializeComponent();
+            this.DataContext = WorkMenuViewModel.Instance;
         }
     }
 }
