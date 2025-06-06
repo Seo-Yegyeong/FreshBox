8abd3f45c503fb20ae5cdbd84bbeabf35b7094ae
Seo-Yegyeong <seoye6618@gmail.com>
2025-06-06 18:13:25 +0900

    feat: In progress - make files related with warehouse

--- FreshBox/Models/Warehouse.cs
+++ FreshBox/Models/Warehouse.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FreshBox.Enums;
 
 namespace FreshBox.Models
 {
-    public enum TempControl
-    {
-        냉동,
-        냉장,
-        실온
-    }
-
     public class Warehouse
     {
         public int Id { get; set; }
-        public string WarehouseName { get; set; } = string.Empty;
-        public string? Location { get; set; }
-        public TempControl TempControl { get; set; }
-        public int? Capacity { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public StorageTemp TempControl { get; set; }
+
+        public Warehouse(int id, string location, string tempControl) {
+            Id = id;
+            Location = location;
+
+            switch (tempControl)
+            {
+                case "냉장":
+                    TempControl = StorageTemp.냉장;
+                    break;
+                case "냉동":
+                    TempControl = StorageTemp.냉동;
+                    break;
+                case "실온":
+                    TempControl = StorageTemp.실온;
+                    break;
+                default:
+                    throw new Exception("Error: 잘못된 옵션입니다.");
+            }
+        }
     }
 }
